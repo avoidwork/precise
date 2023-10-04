ba9ae0d8ff11d24f74a00b778eb1415672572504
Jason Mulligan <jason.mulligan@avoidwork.com>
2023-10-04 07:01:16 -0400

    Updating types with manual specification

--- types/precise.d.ts
+++ types/precise.d.ts
@@ -1,8 +1,8 @@
 export function precise(): Precise;
 export class Precise {
-    started: any[];
-    stopped: any[];
-    diff(ms?: boolean): any;
+    started: number[];
+    stopped: number[];
+    diff(ms?: boolean): number;
     start(): this;
     stop(): this;
 }
