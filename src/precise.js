8414e34e4f444d93efe05c4cc7950ed0c30f7be8
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-03-28 17:22:09 -0400

    Add JSDoc docblocks to Precise class and functions

--- src/precise.js
+++ src/precise.js
@@ -1,62 +1,94 @@
-import {hrtime} from "node:process";
-import {BIG_INT_NEG_1, NOT_STARTED, NOT_STOPPED, STARTED, STOPPED} from "./constants.js";
+import { hrtime } from "node:process";
+import { BIG_INT_NEG_1, NOT_STARTED, NOT_STOPPED, STARTED, STOPPED } from "./constants.js";
 
+/**
+ * High-precision timer using process.hrtime.bigint()
+ * @class
+ */
 export class Precise {
-	constructor () {
-		this.started = BIG_INT_NEG_1;
-		this.stopped = BIG_INT_NEG_1;
-	}
+  /**
+   * Creates a new Precise timer instance
+   * @constructor
+   */
+  constructor() {
+    this.started = BIG_INT_NEG_1;
+    this.stopped = BIG_INT_NEG_1;
+  }
 
-	diff (ms = false) {
-		if (this.started === BIG_INT_NEG_1) {
-			throw new Error(NOT_STARTED);
-		}
+  /**
+   * Returns the time delta between start() and stop()
+   * @param {boolean} [ms=false] - If true, returns milliseconds instead of nanoseconds
+   * @returns {number} Time delta in nanoseconds or milliseconds
+   * @throws {Error} If timer has not been started or stopped
+   */
+  diff(ms = false) {
+    if (this.started === BIG_INT_NEG_1) {
+      throw new Error(NOT_STARTED);
+    }
 
-		if (this.stopped === BIG_INT_NEG_1) {
-			throw new Error(NOT_STOPPED);
-		}
+    if (this.stopped === BIG_INT_NEG_1) {
+      throw new Error(NOT_STOPPED);
+    }
 
-		let result = Number(this.stopped - this.started);
+    let result = Number(this.stopped - this.started);
 
-		if (ms) {
-			result = parseInt(result / 1e6, 10);
-		}
+    if (ms) {
+      result = parseInt(result / 1e6, 10);
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	reset () {
-		this.started = BIG_INT_NEG_1;
-		this.stopped = BIG_INT_NEG_1;
+  /**
+   * Resets the timer to its initial state
+   * @returns {Precise} Returns this for chaining
+   */
+  reset() {
+    this.started = BIG_INT_NEG_1;
+    this.stopped = BIG_INT_NEG_1;
 
-		return this;
-	}
+    return this;
+  }
 
-	start () {
-		if (this.started > BIG_INT_NEG_1) {
-			throw new Error(STARTED);
-		}
+  /**
+   * Starts the timer
+   * @returns {Precise} Returns this for chaining
+   * @throws {Error} If timer has already been started
+   */
+  start() {
+    if (this.started > BIG_INT_NEG_1) {
+      throw new Error(STARTED);
+    }
 
-		this.started = hrtime.bigint();
+    this.started = hrtime.bigint();
 
-		return this;
-	}
+    return this;
+  }
 
-	stop () {
-		if (this.started === BIG_INT_NEG_1) {
-			throw new Error(NOT_STARTED);
-		}
+  /**
+   * Stops the timer
+   * @returns {Precise} Returns this for chaining
+   * @throws {Error} If timer has not been started or has already been stopped
+   */
+  stop() {
+    if (this.started === BIG_INT_NEG_1) {
+      throw new Error(NOT_STARTED);
+    }
 
-		if (this.stopped > BIG_INT_NEG_1) {
-			throw new Error(STOPPED);
-		}
+    if (this.stopped > BIG_INT_NEG_1) {
+      throw new Error(STOPPED);
+    }
 
-		this.stopped = hrtime.bigint();
+    this.stopped = hrtime.bigint();
 
-		return this;
-	}
+    return this;
+  }
 }
 
-export function precise () {
-	return new Precise();
+/**
+ * Factory function that creates a new Precise timer instance
+ * @returns {Precise} New Precise instance
+ */
+export function precise() {
+  return new Precise();
 }
