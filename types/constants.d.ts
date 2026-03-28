8f2718f0bd791c6c8d0f40af5107c8cd5c6def7d
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-03-28 17:50:40 -0400

    Delete types/constants.d.ts

--- types/constants.d.ts
+++
@@ -1,5 +0,0 @@
-export const STARTED: "Timer has been started";
-export const STOPPED: "Timer has been stopped";
-export const NOT_STARTED: "Timer has not been started";
-export const NOT_STOPPED: "Timer has not been stopped";
-export const BIG_INT_NEG_1: any;
