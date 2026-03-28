7c1d3b871d99b8de169823567d87799b99e91d5f
Jason Mulligan <jason.mulligan@avoidwork.com>
2026-03-28 17:46:53 -0400

    Apply tab formatting per .oxfmtrc.json

--- src/precise.js
+++ src/precise.js
@@ -6,83 +6,83 @@ import { BIG_INT_NEG_1, NOT_STARTED, NOT_STOPPED, STARTED, STOPPED } from "./con
  * @class
  */
 export class Precise {
-  /**
-   * Creates a new Precise timer instance
-   * @constructor
-   */
-  constructor() {
-    this.started = BIG_INT_NEG_1;
-    this.stopped = BIG_INT_NEG_1;
-  }
+	/**
+	 * Creates a new Precise timer instance
+	 * @constructor
+	 */
+	constructor() {
+		this.started = BIG_INT_NEG_1;
+		this.stopped = BIG_INT_NEG_1;
+	}
 
-  /**
-   * Returns the time delta between start() and stop()
-   * @param {boolean} [ms=false] - If true, returns milliseconds instead of nanoseconds
-   * @returns {number} Time delta in nanoseconds or milliseconds
-   * @throws {Error} If timer has not been started or stopped
-   */
-  diff(ms = false) {
-    if (this.started === BIG_INT_NEG_1) {
-      throw new Error(NOT_STARTED);
-    }
+	/**
+	 * Returns the time delta between start() and stop()
+	 * @param {boolean} [ms=false] - If true, returns milliseconds instead of nanoseconds
+	 * @returns {number} Time delta in nanoseconds or milliseconds
+	 * @throws {Error} If timer has not been started or stopped
+	 */
+	diff(ms = false) {
+		if (this.started === BIG_INT_NEG_1) {
+			throw new Error(NOT_STARTED);
+		}
 
-    if (this.stopped === BIG_INT_NEG_1) {
-      throw new Error(NOT_STOPPED);
-    }
+		if (this.stopped === BIG_INT_NEG_1) {
+			throw new Error(NOT_STOPPED);
+		}
 
-    let result = Number(this.stopped - this.started);
+		let result = Number(this.stopped - this.started);
 
-    if (ms) {
-      result = parseInt(result / 1e6, 10);
-    }
+		if (ms) {
+			result = parseInt(result / 1e6, 10);
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  /**
-   * Resets the timer to its initial state
-   * @returns {Precise} Returns this for chaining
-   */
-  reset() {
-    this.started = BIG_INT_NEG_1;
-    this.stopped = BIG_INT_NEG_1;
+	/**
+	 * Resets the timer to its initial state
+	 * @returns {Precise} Returns this for chaining
+	 */
+	reset() {
+		this.started = BIG_INT_NEG_1;
+		this.stopped = BIG_INT_NEG_1;
 
-    return this;
-  }
+		return this;
+	}
 
-  /**
-   * Starts the timer
-   * @returns {Precise} Returns this for chaining
-   * @throws {Error} If timer has already been started
-   */
-  start() {
-    if (this.started > BIG_INT_NEG_1) {
-      throw new Error(STARTED);
-    }
+	/**
+	 * Starts the timer
+	 * @returns {Precise} Returns this for chaining
+	 * @throws {Error} If timer has already been started
+	 */
+	start() {
+		if (this.started > BIG_INT_NEG_1) {
+			throw new Error(STARTED);
+		}
 
-    this.started = hrtime.bigint();
+		this.started = hrtime.bigint();
 
-    return this;
-  }
+		return this;
+	}
 
-  /**
-   * Stops the timer
-   * @returns {Precise} Returns this for chaining
-   * @throws {Error} If timer has not been started or has already been stopped
-   */
-  stop() {
-    if (this.started === BIG_INT_NEG_1) {
-      throw new Error(NOT_STARTED);
-    }
+	/**
+	 * Stops the timer
+	 * @returns {Precise} Returns this for chaining
+	 * @throws {Error} If timer has not been started or has already been stopped
+	 */
+	stop() {
+		if (this.started === BIG_INT_NEG_1) {
+			throw new Error(NOT_STARTED);
+		}
 
-    if (this.stopped > BIG_INT_NEG_1) {
-      throw new Error(STOPPED);
-    }
+		if (this.stopped > BIG_INT_NEG_1) {
+			throw new Error(STOPPED);
+		}
 
-    this.stopped = hrtime.bigint();
+		this.stopped = hrtime.bigint();
 
-    return this;
-  }
+		return this;
+	}
 }
 
 /**
@@ -90,5 +90,5 @@ export class Precise {
  * @returns {Precise} New Precise instance
  */
 export function precise() {
-  return new Precise();
+	return new Precise();
 }
--- test/precise_test.js
+++ test/precise_test.js
@@ -4,151 +4,151 @@ import { precise } from "../dist/precise.cjs";
 import { BIG_INT_NEG_1 } from "../src/constants.js";
 
 beforeEach(function () {
-  this.timer = precise();
+	this.timer = precise();
 });
 
 test("Testing starting state - It should be empty", function () {
-  assert.strictEqual(this.timer.started, BIG_INT_NEG_1, "Should be '-1n'");
-  assert.strictEqual(this.timer.stopped, BIG_INT_NEG_1, "Should be '-1n'");
+	assert.strictEqual(this.timer.started, BIG_INT_NEG_1, "Should be '-1n'");
+	assert.strictEqual(this.timer.stopped, BIG_INT_NEG_1, "Should be '-1n'");
 });
 
 test("Testing starting state - It shouldn't be empty", function () {
-  const timer = this.timer.start();
-  assert.notEqual(this.timer.started, BIG_INT_NEG_1, "Shouldn't be '-1n'");
-  assert.strictEqual(this.timer.stopped, BIG_INT_NEG_1, "Should be '-1n'");
-  assert.throws(
-    function () {
-      timer.start();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
+	const timer = this.timer.start();
+	assert.notEqual(this.timer.started, BIG_INT_NEG_1, "Shouldn't be '-1n'");
+	assert.strictEqual(this.timer.stopped, BIG_INT_NEG_1, "Should be '-1n'");
+	assert.throws(
+		function () {
+			timer.start();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
 });
 
 test("Testing stopping state - It shouldn't be empty", function () {
-  const timer = this.timer;
-  assert.throws(
-    function () {
-      timer.diff();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  assert.throws(
-    function () {
-      timer.stop();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  this.timer.start();
-  assert.throws(
-    function () {
-      timer.diff();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  this.timer.stop();
-  assert.notEqual(this.timer.started, BIG_INT_NEG_1, "Shouldn't be '-1n'");
-  assert.notEqual(this.timer.started, BIG_INT_NEG_1, "Shouldn't be '-1n'");
-  assert.throws(
-    function () {
-      timer.stop();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
+	const timer = this.timer;
+	assert.throws(
+		function () {
+			timer.diff();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	assert.throws(
+		function () {
+			timer.stop();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	this.timer.start();
+	assert.throws(
+		function () {
+			timer.diff();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	this.timer.stop();
+	assert.notEqual(this.timer.started, BIG_INT_NEG_1, "Shouldn't be '-1n'");
+	assert.notEqual(this.timer.started, BIG_INT_NEG_1, "Shouldn't be '-1n'");
+	assert.throws(
+		function () {
+			timer.stop();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
 });
 
 test("Testing difference - It should return an integer", function () {
-  const timer = this.timer;
-  assert.throws(
-    function () {
-      timer.diff();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  assert.throws(
-    function () {
-      timer.diff();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  this.timer.start().stop();
-  assert.strictEqual(typeof this.timer.diff(), "number", "Should be 'number'");
-  assert.strictEqual(typeof this.timer.diff(true), "number", "Should be 'number'");
-  assert.strictEqual(this.timer.diff() > this.timer.diff(true), true, "Should be 'true'");
-  assert.strictEqual(
-    parseInt(this.timer.diff() / 1e6, 10),
-    this.timer.diff(true),
-    "Should be 'true'",
-  );
+	const timer = this.timer;
+	assert.throws(
+		function () {
+			timer.diff();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	assert.throws(
+		function () {
+			timer.diff();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	this.timer.start().stop();
+	assert.strictEqual(typeof this.timer.diff(), "number", "Should be 'number'");
+	assert.strictEqual(typeof this.timer.diff(true), "number", "Should be 'number'");
+	assert.strictEqual(this.timer.diff() > this.timer.diff(true), true, "Should be 'true'");
+	assert.strictEqual(
+		parseInt(this.timer.diff() / 1e6, 10),
+		this.timer.diff(true),
+		"Should be 'true'",
+	);
 });
 
 test("Testing difference - It should have different values when started & stopped at different times", async function () {
-  this.timer.start();
-  await new Promise((resolve) => setTimeout(resolve, 250));
-  this.timer.stop();
-  assert.strictEqual(this.timer.stopped > this.timer.started, true, "Should be 'true'");
+	this.timer.start();
+	await new Promise((resolve) => setTimeout(resolve, 250));
+	this.timer.stop();
+	assert.strictEqual(this.timer.stopped > this.timer.started, true, "Should be 'true'");
 });
 
 test("Testing chaining - It should be chainable", function () {
-  const timer = this.timer;
-  assert.strictEqual(timer.start(), timer, "Should be 'timer'");
-  assert.strictEqual(timer.stop(), timer, "Should be 'timer'");
+	const timer = this.timer;
+	assert.strictEqual(timer.start(), timer, "Should be 'timer'");
+	assert.strictEqual(timer.stop(), timer, "Should be 'timer'");
 });
 
 test("Testing invalid states - It should throw an error", function () {
-  const timer = this.timer;
-  assert.throws(
-    function () {
-      timer.stop();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  assert.throws(
-    function () {
-      timer.diff();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  this.timer.start();
-  assert.throws(
-    function () {
-      timer.start();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
-  this.timer.stop();
-  assert.throws(
-    function () {
-      timer.stop();
-    },
-    Error,
-    "Should be an 'Error'",
-  );
+	const timer = this.timer;
+	assert.throws(
+		function () {
+			timer.stop();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	assert.throws(
+		function () {
+			timer.diff();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	this.timer.start();
+	assert.throws(
+		function () {
+			timer.start();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
+	this.timer.stop();
+	assert.throws(
+		function () {
+			timer.stop();
+		},
+		Error,
+		"Should be an 'Error'",
+	);
 });
 
 test("Testing precision - It should be precise", async function () {
-  const timer = this.timer;
-  timer.start();
-  await new Promise((resolve) => setTimeout(resolve, 250));
-  timer.stop();
-  assert.strictEqual(timer.diff() > 0, true, "Should be 'true'");
-  assert.strictEqual(timer.diff(true) > 0, true, "Should be 'true'");
+	const timer = this.timer;
+	timer.start();
+	await new Promise((resolve) => setTimeout(resolve, 250));
+	timer.stop();
+	assert.strictEqual(timer.diff() > 0, true, "Should be 'true'");
+	assert.strictEqual(timer.diff(true) > 0, true, "Should be 'true'");
 });
 
 test("Testing reset - It should reset", function () {
-  const timer = this.timer;
-  assert.strictEqual(timer.start(), timer, "Should be 'timer'");
-  assert.strictEqual(timer.stop(), timer, "Should be 'timer'");
-  assert.strictEqual(timer.reset(), timer, "Should be 'timer'");
-  assert.strictEqual(timer.started, BIG_INT_NEG_1, "Should be '-1n'");
-  assert.strictEqual(timer.stopped, BIG_INT_NEG_1, "Should be '-1n'");
+	const timer = this.timer;
+	assert.strictEqual(timer.start(), timer, "Should be 'timer'");
+	assert.strictEqual(timer.stop(), timer, "Should be 'timer'");
+	assert.strictEqual(timer.reset(), timer, "Should be 'timer'");
+	assert.strictEqual(timer.started, BIG_INT_NEG_1, "Should be '-1n'");
+	assert.strictEqual(timer.stopped, BIG_INT_NEG_1, "Should be '-1n'");
 });
